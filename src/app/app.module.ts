a8e393e4424db4339a31860fc486a971d4d69ea4
tiihmesquita <thiagomesquita17@gmail.com>
2022-10-24 09:42:48 -0300

    data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+
 @NgModule({
   declarations: [
     AppComponent,
